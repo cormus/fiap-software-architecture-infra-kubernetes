73600373740296f12f26aa2465290839d48a5c8b
Alex Ribeiro <alexprodutor.com@gmail.com>
2025-05-15 08:46:28 -0300

    teste conexão eks

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,38 @@
-# resource "aws_s3_bucket" "bucket" {
-#   bucket = var.bucket_name
-# }
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.0"
+  name    = "eks-vpc"
+  cidr    = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.3.0/24", "10.0.4.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+  tags = {
+    Name = "eks-vpc"
+  }
+}
+
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = "springboot-eks"
+  cluster_version = "1.32"
+  subnets         = module.vpc.private_subnets
+  vpc_id          = module.vpc.vpc_id
+
+  eks_managed_node_groups = {
+    default = {
+      desired_capacity = 2
+      max_capacity     = 3
+      min_capacity     = 1
+
+      instance_types = ["a1.medium"]
+    }
+  }
+}
