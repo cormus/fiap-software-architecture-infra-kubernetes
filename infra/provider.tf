da166b7050e30a6b71185cfe0377bef28cec1547
Alex Ribeiro <alexprodutor.com@gmail.com>
2025-05-16 14:30:36 -0300

    teste de provider?

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,3 +1,16 @@
 provider "aws" {
   region = "us-east-2"
 }
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
+    }
+  }
+}
