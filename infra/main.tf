21d390598845451e4bd6426e933cff7b7832e409
Alex Ribeiro <alexprodutor.com@gmail.com>
2025-05-16 16:37:28 -0300

    teste auto

--- infra/main.tf
+++ infra/main.tf
@@ -48,28 +48,43 @@ module "eks" {
             instance_types = ["t3.medium"]
         }
     }
-}
 
-module "aws_auth" {
-  source = "terraform-aws-modules/eks/aws//modules/aws-auth"
-  manage_aws_auth_configmap = true
+    manage_aws_auth_configmap = true
 
-  depends_on = [module.eks]
-
-  aws_auth_roles = [
-  {
-    rolearn  = "arn:aws:iam::881307377501:role/github-actions-fiap-pipelike"
-    username = "role1"
-    groups   = ["system:masters"]
-  }
-]
-
-  aws_auth_users = [
+    aws_auth_users = [
     {
-      userarn  = "arn:aws:iam::881307377501:user/terraformUser"
+      userarn  = "arn:aws:iam::66666666666:user/user1"
       username = "user1"
       groups   = ["system:masters"]
-    }
+    },
+    {
+      userarn  = "arn:aws:iam::66666666666:user/user2"
+      username = "user2"
+      groups   = ["system:masters"]
+    },
   ]
 }
+
+# module "aws_auth" {
+#   source = "terraform-aws-modules/eks/aws//modules/aws-auth"
+#   manage_aws_auth_configmap = true
+
+#   depends_on = [module.eks]
+
+#   aws_auth_roles = [
+#   {
+#     rolearn  = "arn:aws:iam::881307377501:role/github-actions-fiap-pipelike"
+#     username = "role1"
+#     groups   = ["system:masters"]
+#   }
+# ]
+
+#   aws_auth_users = [
+#     {
+#       userarn  = "arn:aws:iam::881307377501:user/terraformUser"
+#       username = "user1"
+#       groups   = ["system:masters"]
+#     }
+#   ]
+# }
  
