37fa32e4e7297cf4418a4a9f469ea4a994446660
Alex Ribeiro <alexprodutor.com@gmail.com>
2025-05-16 15:02:36 -0300

    teste criar EKS

--- infra/main.tf
+++ infra/main.tf
@@ -54,6 +54,8 @@ module "aws_auth" {
   source = "terraform-aws-modules/eks/aws//modules/aws-auth"
   manage_aws_auth_configmap = true
 
+  depends_on = [module.eks]
+
   aws_auth_roles = [
   {
     rolearn  = "arn:aws:iam::881307377501:role/github-actions-fiap-pipelike"
