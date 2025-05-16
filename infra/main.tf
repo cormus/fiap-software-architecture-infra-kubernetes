ae97c2f4f97814ec4a973a47b9b42f489eec00a3
Alex Ribeiro <alexprodutor.com@gmail.com>
2025-05-16 16:27:07 -0300

    original

--- infra/main.tf
+++ infra/main.tf
@@ -1,9 +1,60 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.0"
+  name    = "eks-vpc"
+  cidr    = "10.0.0.0/16"
+
+  azs             = ["us-east-2a", "us-east-2b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.3.0/24", "10.0.4.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+  tags = {
+    Name = "eks-vpc"
+  }
+}
+
+module "eks" {
+    source          = "terraform-aws-modules/eks/aws"
+    version         = "19.15.1"
+
+    cluster_name                    = "springboot-eks"
+    cluster_endpoint_public_access  = true
+
+    cluster_addons = {
+        coredns = {
+            most_recent = true
+        }
+        kube-proxy = {
+            most_recent = true
+        }
+        vpc-cni = {
+            most_recent = true
+        }
+    }
+
+    vpc_id          = module.vpc.vpc_id
+    subnet_ids      = module.vpc.private_subnets 
+
+    # Acesso ao endpoint da API EKS
+
+    eks_managed_node_groups = {
+        default = {
+            desired_capacity = 2
+            max_capacity     = 3
+            min_capacity     = 1
+
+            instance_types = ["t3.medium"]
+        }
+    }
+}
 
 module "aws_auth" {
   source = "terraform-aws-modules/eks/aws//modules/aws-auth"
   manage_aws_auth_configmap = true
 
-  ##depends_on = [module.eks]
+  depends_on = [module.eks]
 
   aws_auth_roles = [
   {
