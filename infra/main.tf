818b56646838fdb0fbeb25304294baafcaa8405f
Alex Ribeiro <alexprodutor.com@gmail.com>
2025-05-16 14:23:03 -0300

    teste de permissão de usuário

--- infra/main.tf
+++ infra/main.tf
@@ -14,7 +14,7 @@ module "vpc" {
     Name = "eks-vpc"
   }
 }
- 
+
 module "eks" {
     source          = "terraform-aws-modules/eks/aws"
     version         = "19.15.1"
@@ -49,3 +49,25 @@ module "eks" {
         }
     }
 }
+
+module "aws_auth" {
+  source = "terraform-aws-modules/eks/aws//modules/aws-auth"
+  manage_aws_auth_configmap = true
+
+  aws_auth_roles = [
+  {
+    rolearn  = "arn:aws:iam::881307377501:role/github-actions-fiap-pipelike"
+    username = "role1"
+    groups   = ["system:masters"]
+  }
+]
+
+  aws_auth_users = [
+    {
+      userarn  = "arn:aws:iam::881307377501:user/terraformUser"
+      username = "user1"
+      groups   = ["system:masters"]
+    }
+  ]
+}
+ 
