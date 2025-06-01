868cde772af58b1178266e2c5eac284464095888
Alex Ribeiro <alexprodutor.com@gmail.com>
2025-06-01 09:33:50 -0300

    Criar ECR

--- infra/main.tf
+++ infra/main.tf
@@ -93,3 +93,12 @@ module "eks" {
 # }
  
 
+resource "aws_ecr_repository" "app_repo" {
+  name = var.ecr_repository_name
+  image_tag_mutability = "MUTABLE"
+  force_delete         = true
+}
+
+output "repository_url" {
+  value = aws_ecr_repository.app_repo.repository_url
+}
