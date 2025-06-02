f8660b0baa77cbcf43469cece7f2e99eb0275355
Alex Ribeiro <alexprodutor.com@gmail.com>
2025-06-02 08:25:19 -0300

    Removido conteúdo sem utilidade

--- infra/provider.tf
+++ infra/provider.tf
@@ -26,9 +26,3 @@ terraform {
     }
   }
 }
-
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-}
